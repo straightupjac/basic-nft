d76dac095b7c171ee0227104ed83f7e996c0961b
straightupjac <16831223+straightupjac@users.noreply.github.com>
2021-11-10 12:52:55 -0500

    update metadata

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@
 */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-const { RINKEBY_API_URL, MAINNET_API_URL, PRIVATE_KEY } = process.env;
+const { RINKEBY_API_URL, ROPSTEN_API_URL, MAINNET_API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
    solidity: "0.8.0",
    defaultNetwork: "rinkeby",
@@ -15,6 +15,10 @@ module.exports = {
       },
       mainnet: {
         url: MAINNET_API_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+     },
+     ropsten: {
+        url: ROPSTEN_API_URL,
         accounts: [`0x${PRIVATE_KEY}`]
      }
    },
